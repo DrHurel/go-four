c790dd39921badfa3d5e3c0dbd94fef04fa07405
DrHurel <jeremyhurel34@gmail.com>
2023-08-18 12:46:53 +0200

    save

--- frontend/pages/connect-four/offline.vue
+++ frontend/pages/connect-four/offline.vue
@@ -1,37 +1,38 @@
 <template>
-  <header class="flex center width-100">
+  <header class="flex center w-100">
 
     <div class="flex align-center jsb">
       <button @click="menu">Menu</button>
-      <img src="/images/logo.svg" alt="">
+      <img src="/images/logo.svg" alt="logo">
       <button @click="newgame">restart</button>
     </div>
   </header>
-  <main class="grid center width-100">
+  <main class="grid center w-100">
 
     <section class="flex column center">
       <div class="score-container flex column center">
-        <img src="/images/player-one.svg" alt="">
+        <img src="/images/player-one.svg" alt="p1">
         <p class="player">player 1</p>
         <p class="p-3-5rem">{{
           score[0]
         }}</p>
       </div>
     </section>
-    <section class="board grid center height-100 width-100">
-      <div class="cursor grid center width-100">
+    <section class="board grid center h-100 w-100">
+      <div class="cursor grid center w-100">
         <span :style="{
               transform: `translateX(calc(${cursor} * 100%))`
-            }" class="flex center width-100">
-          <img v-bind:src="player == Player.PLAYER2 ? '/images/marker-yellow.svg' : '/images/marker-red.svg'" />
+            }" class="flex center w-100">
+          <img v-bind:src="player == Player.PLAYER2 ? '/images/marker-yellow.svg' : '/images/marker-red.svg'"
+            alt="cursor" />
         </span>
       </div>
-      <img class="front-board height-100" src="/images/board-layer-white-large.svg" alt="" />
-      <div class="inner-board grid center">
+      <img class="front-board h-100 g-2-3" src="/images/board-layer-white-large.svg" alt="" />
+      <div class="inner-board grid center g-2-3">
         <img class="square" v-for="(item, _) in  board " :class="{ clean: (item == Player.EMPTY) }"
           v-bind:src="(item == Player.PLAYER2) ? '/images/counter-yellow-large.svg' : '/images/counter-red-large.svg'" />
       </div>
-      <img class="back-board height-100" src="/images/board-layer-black-large.svg" alt="" />
+      <img class="back-board h-100 g-2-3" src="/images/board-layer-black-large.svg" alt="" />
 
     </section>
     <section class="flex column center">
@@ -46,14 +47,14 @@
 
 
   </main>
-  <footer class="flex center width-100">
+  <footer class="flex center w-100">
     <div class="grid center timer">
 
-      <img
+      <img class="g-1-2"
         v-bind:src="(player == Player.PLAYER2) ? '/images/turn-background-yellow.svg' : '/images/turn-background-red.svg'"
         alt="">
 
-      <div class="flex center column">
+      <div class="flex center column g-1-2">
         <p>YOUR TURN</p>
         <p class="p-3-5rem">{{ timer }}s</p>
       </div>
@@ -115,11 +116,6 @@ onBeforeUnmount(
 .flex {
   display: flex;
 
-
-  &.align-center {
-    align-items: center;
-  }
-
   &.column {
     flex-direction: column;
   }
@@ -129,12 +125,27 @@ onBeforeUnmount(
   }
 }
 
+.align-center {
+  align-items: center;
+}
+
 .grid {
   display: grid;
 }
 
-.center {
+.g-2-3 {
+  grid-row: 2/3;
+  grid-column: 2/3;
+}
 
+.g-1-2 {
+  grid-row: 1/2;
+  grid-column: 1/2;
+}
+
+
+
+.center {
   align-items: center;
   justify-content: center;
 }
@@ -143,22 +154,19 @@ onBeforeUnmount(
   aspect-ratio: 1/1;
 }
 
-.height-100 {
+.h-100 {
   height: 100%;
 }
 
-.width-100 {
+.w-100 {
   width: 100%;
 }
 
 header {
-
   background-color: var(--purple);
-
   height: 8vh;
 
   div {
-
 
     height: fit-content;
     width: 39.5rem;
@@ -182,39 +190,32 @@ header {
   }
 }
 
+
+
 .board {
 
   grid-template-columns: 1fr 39.5rem 1fr;
   grid-template-rows: 1fr 36.5rem 1fr;
 
   .front-board {
-
-    z-index: 2;
-    grid-row: 2/3;
-    grid-column: 2/3;
+    z-index: 3;
   }
 
   .back-board {
-    grid-row: 2/3;
-    grid-column: 2/3;
-    z-index: 0;
+
+    z-index: 1;
+    transform: translateX(0.5rem) translateY(0.5rem);
 
   }
 
   .inner-board {
     width: calc(39.5rem - 2.5rem);
     height: calc(36.5rem - 3.75rem);
-    padding-bottom: 3.5rem;
-    padding-left: 1.25rem;
-    padding-right: 1.25rem;
-    z-index: 1;
-    grid-column: 2/3;
-    grid-row: 2/3;
-
+    padding: 1.25rem 1.25rem 3.5rem 1.25rem;
+    z-index: 2;
     grid-template-columns: repeat(7, 1fr);
     grid-template-rows: repeat(6, 1fr);
-
-
+    ;
 
     img {
       z-index: 1;
@@ -235,8 +236,6 @@ header {
   }
 }
 
-
-
 .score-container {
 
   background-color: white;
@@ -250,8 +249,6 @@ header {
     position: absolute;
     transform: translateY(-5rem);
   }
-
-
 }
 
 p {
@@ -265,8 +262,6 @@ p {
   font-weight: 700;
   line-height: normal;
 }
-
-
 
 .cursor {
 
@@ -282,21 +277,14 @@ p {
   }
 }
 
-
-
 main {
-
   grid-template-columns: 1fr 50% 1fr;
-
   height: 92vh;
   background-color: var(--purple);
-
 }
-
 
 footer {
   position: absolute;
-
   bottom: 0;
   height: 18vh;
   background-color: var(--dark-purple);
@@ -306,20 +294,14 @@ footer {
 
 .timer {
   z-index: 3;
-
   grid-template-columns: 1fr;
   grid-template-rows: 1fr;
 
-
   img {
-    grid-column: 1/2;
-    grid-row: 1/2;
     width: 9.75rem;
   }
 
   div {
-    grid-column: 1/2;
-    grid-row: 1/2;
     color: white;
 
     p:first-child {
@@ -328,9 +310,6 @@ footer {
       font-weight: 700;
       line-height: normal;
     }
-
-
-
   }
 }
 
